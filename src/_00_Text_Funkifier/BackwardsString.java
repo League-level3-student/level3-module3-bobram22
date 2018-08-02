27a9e33ac3f634d6917c77ebec15a87255b55d2b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-01 18:29:01 -0700

    Started Text Funkifier

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -8,7 +8,9 @@ public class BackwardsString extends SpecialString {
 
 	@Override
 	public String funkifyText(String s) {
-		
+		for(int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+		}
 		return "";
 	}
 
